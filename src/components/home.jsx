b410be0deae96251222ff2b076173b4f221ce111
clemon <631434851@qq.com>
2019-08-29 00:50:50 +0800

    finish homepage

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import cssobj from '@/css/home.module.scss'
+import '@/css/home.scss'
 const axios = require('axios');
 
 
@@ -15,6 +15,7 @@ class Home extends React.Component {
         let api = 'http://a.itying.com/api/productlist';
         axios.get(api).then((res) => {
                                         this.setState({list: res.data.result});
+                                        // console.log(this.state.list["0"].list);
                                     }).catch(function(err) {console.log(err)});
     }
 
@@ -22,13 +23,37 @@ class Home extends React.Component {
         this.requestData();
     }
 
+    genLi = (value, key) => {
+        return (
+            <li key={key}>
+                <div className="inner">
+                    <img src={require('../images/2.jpg')} alt="" />
+                    <p className="title">{value.title}</p>
+                    <p className="price">价格:{value.price}</p>
+                </div>
+            </li>
+        )
+    }
+
+    genClass = (value, key) => {
+        return (
+            <div key={key} className="item">
+            <h3 className="item_cate">{value.title}</h3>
+            <ul className="item_list">
+                {value.list.map((value, key) => this.genLi(value, key))}
+            </ul>
+            </div>
+        )
+    }
+
     render() {
         return (
-            <div className="item">
-                <h3 className={cssobj.itemClassName}>className</h3>
-                <ul>
-                    <li>1</li>
-                </ul>
+            <div className="home">
+                <div className="list">
+                {
+                    this.state.list.map( (value, key) => this.genClass(value, key) )
+                }
+                </div> 
             </div>
         );
     }
