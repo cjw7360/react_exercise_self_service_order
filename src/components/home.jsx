947430cc6270e2851d25228a847e191ed3f2b274
clemon <631434851@qq.com>
2019-08-28 23:19:50 +0800

    add scss

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,16 +1,35 @@
 import React from 'react';
+import cssobj from '@/css/home.module.scss'
+const axios = require('axios');
+
 
 class Home extends React.Component {
     constructor(props) {
         super(props);
-        this.state={};
+        this.state={
+            list: [],
+        };
     }
 
+    requestData = () => {
+        let api = 'http://a.itying.com/api/productlist';
+        axios.get(api).then((res) => {
+                                        this.setState({list: res.data.result});
+                                    }).catch(function(err) {console.log(err)});
+    }
+
+    componentDidMount() {
+        this.requestData();
+    }
 
     render() {
         return (
-            <div>home component!</div>
-            
+            <div className="item">
+                <h3 className={cssobj.itemClassName}>className</h3>
+                <ul>
+                    <li>1</li>
+                </ul>
+            </div>
         );
     }
 }
